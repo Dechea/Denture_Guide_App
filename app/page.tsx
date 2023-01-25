939a18fa66137dee5971c6c8197a9d28a5298feb
Aditya Vaishnav <firehudson@gmail.com>
2023-01-25 13:44:51 +0000

    Fix `fs not found` error

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-/** @jsxImportSource @emotion/react */
-'use client';
+export const runtime = 'nodejs';
 
-import { Link } from '../components/Reshaped/Reshaped';
+import { Notes } from "../components/Notes";
+import { Link } from "../components/Reshaped/Reshaped";
 
 export default function Home() {
   return (
-    <Link href='/teeth-diagram'>Teeth Diagram</Link>
+    <>
+      <Link href="/teeth-diagram">Teeth Diagram</Link>
+      <Notes />
+    </>
   );
 }
--- next.config.js
+++ next.config.js
@@ -2,13 +2,24 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    runtime: 'experimental-edge',
+    runtime: "experimental-edge",
   },
-}
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped. Doesn't make much sense, but how it is
+      // the solution
+      fs: false,
+    };
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+    return config;
+  },
+  
+};
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = withBundleAnalyzer(nextConfig);
