df58b71f4da03345d08430224469122a7918a889
Aditya Vaishnav <firehudson@gmail.com>
2023-01-25 15:23:22 +0000

    Add fauna token from env

--- components/Notes.tsx
+++ components/Notes.tsx
@@ -2,18 +2,17 @@ import { fql } from "fauna";
 import { client } from "../src/fauna";
 
 export const Notes = () => {
-  const collections = async() => {
-   try {
-    const res = await client.query(fql``)
+  const collections = async () => {
+    try {
+      const res = await client.query(fql`Collection.create({name: "Note"})`);
 
-    console.log({res})
-   } catch (error) {
-    console.log({error})
-   }
+      console.log({ res });
+    } catch (error) {
+      console.log({ error });
+    }
   };
 
-  collections()
-
+  collections();
 
   return <div>Notes</div>;
 };
--- src/fauna/index.ts
+++ src/fauna/index.ts
@@ -1,9 +1,10 @@
-import { Client } from "fauna";
+import { Client, endpoints } from "fauna";
 
 const client: Client = new Client({
-  secret: "fnAE7JmM5qAA19TSJxunLNLYFCqETG_c-NDMc83o",
-  endpoint: new URL("https://db.eu.fauna.com/"),
-  timeout_ms: 60_000,
+  secret: process.env.FAUNA_DECHEA_ENV__ADMIN__TOKEN,
+  // endpoint: new URL("https://db.eu.fauna.com/"),
+  endpoint: endpoints.preview,
+
 });
 
 export { client };
