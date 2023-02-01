4249cbd5b0da09754a87bed02ddc0e63db30028e
Aditya Vaishnav <firehudson@gmail.com>
2023-02-01 11:51:38 +0000

    Add useQuery hook for get data from fauna

--- components/Notes/Notes.tsx
+++ components/Notes/Notes.tsx
@@ -1,14 +1,54 @@
-'use client';
+"use client";
 
-import Fetcher from './Fetcher';
-import { NotesList } from './NotesList';
+import { ChangeEvent, useState } from "react";
+import useQuery, { useQueryReturnType } from "../../src/fauna/useQuery";
+
+type queryReturnType = useQueryReturnType & { data: { text: string }[] }
 
 export default function Notes() {
+  const [note, setNote] = useState("");
+
+  const { data, isLoading, addData, resetData } = useQuery({
+    query: "Note.all.paginate(500)",
+  }) as queryReturnType;
+
+  const onChangeNote = (e: ChangeEvent<HTMLInputElement>) => {
+    setNote(e.target.value);
+  };
+
+  const onClickCreateNoteButton = () => {
+    addData([...data, { text: note }]);
+
+    fetch("/api/notes/createNote", {
+      method: "POST",
+      body: JSON.stringify({ text: note }),
+    })
+      .then((res) => res.json())
+      .catch((error) => {
+        resetData(data, note, "text");
+        console.log({ error });
+      })
+      .finally(() => {
+        setNote("");
+      });
+  };
+
   return (
-    // Below error resolution is not supported in next 13 so we need to disable it.
-    // @ts-expect-error
-    <Fetcher query={`Note.all.paginate(500)`}>
-      <NotesList data={[]} addData={() => {}} resetData={() => {}} />
-    </Fetcher>
+    <>
+      {isLoading ? (
+        <div>...Loading</div>
+      ) : (
+        <>
+          {data?.map((note, index) => (
+            <div key={index}>{note.text}</div>
+          ))}
+
+          <div>
+            <input onChange={onChangeNote} value={note} />
+            <button onClick={onClickCreateNoteButton}>Add Note</button>
+          </div>
+        </>
+      )}
+    </>
   );
 }
