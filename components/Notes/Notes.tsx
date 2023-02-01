98dc3c4f3d5bb1c158e29802eec1c5b4d64ad020
Madhav <madhav.kabra@dechea.com>
2023-02-01 05:18:15 +0000

    Add comment

--- components/Notes/Notes.tsx
+++ components/Notes/Notes.tsx
@@ -5,6 +5,7 @@ import { NotesList } from './NotesList';
 
 export default function Notes() {
   return (
+    // Below error resolution is not supported in next 13 so we need to disable it.
     // @ts-expect-error
     <Fetcher query={`Note.all.paginate(500)`}>
       <NotesList data={[]} addData={() => {}} resetData={() => {}} />
