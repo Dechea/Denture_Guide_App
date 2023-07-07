a72235dc33df8a352c5ab94ea92d1c8a89c48e81
bohdank1995 <bohdan@karpliak.me>
2023-07-07 08:17:06 +0000

    Badge updated

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -34,61 +34,62 @@ export default function Header({ patientFileId }: HeaderProps) {
 
   return (
     <View
-      align='stretch'
-      width='100%'
-      position='sticky'
+      align="stretch"
+      width="100%"
+      position="sticky"
       insetTop={0}
       zIndex={50}
-      backgroundColor='white'
+      backgroundColor="white"
     >
       <View
-        direction='row'
-        align='center'
+        direction="row"
+        align="center"
         paddingBlock={2}
         paddingInline={6}
-        className='!justify-between'
+        className="!justify-between"
       >
         <Button
           icon={<Icon svg={MenuIcon} size={4} />}
-          variant='ghost'
+          variant="ghost"
           onClick={() => router.push('/')}
         >
-          <Text variant='body-3' weight='medium'>
+          <Text variant="body-3" weight="medium">
             Orders List
           </Text>
         </Button>
 
-        <View direction='row' gap={3} align='center'>
+        <View direction="row" gap={3} align="center">
           <Avatar
             src={patientFile?.patient?.avatar || '/defaultAvatar.svg'}
             size={8}
           />
-          <Text variant='body-2' weight='medium'>
+          <Text variant="body-2" weight="medium">
             {patientFile?.patient?.name}
           </Text>
         </View>
 
-        <View direction='row' align='center' justify='end' gap={6}>
-          <View direction='row' align='center' gap={2.5}>
+        <View direction="row" align="center" justify="end" gap={3}>
+          <View direction="row" align="center" gap={2.5}>
             <Button
-              variant='ghost'
+              variant="ghost"
               icon={<Icon svg={CartIcon} size={4} />}
               highlighted={pathname === `/${patientFileId}/cart`}
               onClick={() => router.push(`/${patientFileId}/cart`)}
             >
-              <Text variant='body-3' weight='medium'>
-                {totalCostOfProductsInCart} €
-              </Text>
+              <View direction="row" align="center" justify="center" gap={2}>
+                <Text variant="body-3" weight="medium">
+                  {totalCostOfProductsInCart} €
+                </Text>
+                <Badge color="critical" size="small">
+                  {totalProductsInCart}
+                </Badge>
+              </View>
             </Button>
-            <Badge color='critical' size='small'>
-              {totalProductsInCart}
-            </Badge>
           </View>
           <Button
             icon={<CostEst />}
-            variant='ghost'
-            size='small'
-            color='neutral'
+            variant="ghost"
+            color="neutral"
             highlighted={pathname === `/costestimation`}
             onClick={() => router.push(`/costestimation`)}
           >
