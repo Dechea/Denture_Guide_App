f480a441266025ba3e6071c41fcdff40c9a88a5c
Madhav <madhav.kabra@dechea.com>
2023-07-17 07:51:30 +0000

    Add UI to show selected material over tooth

---
+++ __mocks__/treatmentTabs.ts
@@ -0,0 +1,340 @@
+export const selectedTeethData = [
+  {
+    name: '47',
+    crown: {
+      treatmentDoc: {
+        treatment: {
+          name: 'artificialCrown',
+        },
+        selectedProducts: [
+          {
+            selectedProduct: {
+              id: '369977684001816781',
+              coll: 'Product',
+              ts: '2023-07-11T18:04:05.970Z',
+              manufacturer: {
+                name: 'CAMLOG',
+              },
+              manufacturerProductId: 'J2244.3802',
+              image: 'https://bucket.dechea.com/product/images/J2244.3802.jpg',
+              validFrom: '',
+              validTo: '',
+              status: 'ACTIVE',
+              localizations: [
+                {
+                  name: '',
+                  description: '',
+                  price: {
+                    amount: 18,
+                    currency: 'EURO',
+                    validFrom: '',
+                    validTo: '',
+                  },
+                  locale: 'FR',
+                },
+                {
+                  name: 'CAMLOG® Modeling aid for CAMLOG® Titanium base CAD/CAM, crown, Ø 3.8, burn-out',
+                  description:
+                    'CAMLOG® Modeling aid for CAMLOG® Titanium base CAD/CAM, burn-out, for fabricating mesostructures and crowns, material: POM.',
+                  price: {
+                    amount: 18,
+                    currency: 'EURO',
+                    validFrom: '',
+                    validTo: '',
+                  },
+                  locale: 'EN',
+                },
+                {
+                  name: 'CAMLOG® Modellierhilfe für CAMLOG® Titanbasis CAD/CAM, Krone, Ø 3.8, ausbrennbar',
+                  description: '',
+                  price: {
+                    amount: 18,
+                    currency: 'EURO',
+                    validFrom: '',
+                    validTo: '',
+                  },
+                  locale: 'DE',
+                },
+              ],
+              abutment: {
+                implantLine: 'CAMLOG',
+                abutmentLine: '',
+                guided: false,
+                platformSwitch: '',
+                retention: '',
+                material: '',
+                sterile: false,
+                connectionType: '',
+                heightProsthetic: '',
+                maxTorque: '',
+                angle: '',
+                diameterPlatform: '',
+                heightGingiva: '',
+                indication: 'CROWN',
+                singleUse: true,
+                workflow: 'ANALOG',
+              },
+            },
+            quantity: 1,
+          },
+        ],
+      },
+    },
+    root: {
+      treatmentDoc: {
+        treatment: {
+          name: 'implant',
+        },
+        selectedProducts: [
+          {
+            selectedProduct: {
+              id: '369977684412858573',
+              coll: 'Product',
+              ts: '2023-07-11T18:04:05.970Z',
+              manufacturer: {
+                name: 'CAMLOG',
+              },
+              manufacturerProductId: 'K1044.3311',
+              image: 'https://bucket.dechea.com/product/images/K1044.3311.jpg',
+              validFrom: '',
+              validTo: '',
+              status: 'ACTIVE',
+              localizations: [
+                {
+                  name: '',
+                  description: '',
+                  price: {
+                    amount: 163,
+                    currency: 'EURO',
+                    validFrom: '',
+                    validTo: '',
+                  },
+                  locale: 'FR',
+                },
+                {
+                  name: 'CAMLOG® SCREW-LINE implant, Promote®, snap-in, Ø 3.3, L 11',
+                  description:
+                    'CAMLOG® SCREW-LINE implant, Promote® plus, incl. insertion post and cover screw, sterile, material: titanium Grade 4. New packaging since June 12, 2017.',
+                  price: {
+                    amount: 163,
+                    currency: 'EURO',
+                    validFrom: '',
+                    validTo: '',
+                  },
+                  locale: 'EN',
+                },
+                {
+                  name: 'CAMLOG® SCREW-LINE Implantat, Promote®, snap-in (gesteckt), Ø 3.3, L 11',
+                  description: '',
+                  price: {
+                    amount: 163,
+                    currency: 'EURO',
+                    validFrom: '',
+                    validTo: '',
+                  },
+                  locale: 'DE',
+                },
+              ],
+              implant: {
+                implantLine: 'CAMLOG',
+                material: 'TITANIUM',
+                level: '',
+                engaging: '',
+                platformSwitch: false,
+                guided: '',
+                insertionPost: '',
+                sterile: true,
+                length: 11,
+                lengthNeck: 1.4,
+                diameterPlatform: 3.3,
+                singleUse: true,
+              },
+            },
+            quantity: 1,
+          },
+        ],
+      },
+    },
+  },
+];
+
+// {
+//     name: '46',
+//     crown: {
+//       treatmentDoc: {
+//         treatment: {
+//           name: 'artificialCrown',
+//         },
+//         selectedProducts: [
+//           {
+//             selectedProduct: {
+//               id: '369977684016496845',
+//               coll: 'Product',
+//               ts: '2023-07-11T18:04:05.970Z',
+//               manufacturer: {
+//                 name: 'CAMLOG',
+//               },
+//               manufacturerProductId: 'J2254.3305',
+//               image: 'https://bucket.dechea.com/product/images/J2254.3305.jpg',
+//               validFrom: '',
+//               validTo: '',
+//               status: 'ACTIVE',
+//               localizations: [
+//                 {
+//                   name: '',
+//                   description: '',
+//                   price: {
+//                     amount: 70,
+//                     currency: 'EURO',
+//                     validFrom: '',
+//                     validTo: '',
+//                   },
+//                   locale: 'FR',
+//                 },
+//                 {
+//                   name: 'CAMLOG® Bar abutment, Ø 3.3, straight, GH 0.5, sterile',
+//                   description:
+//                     'CAMLOG® Bar abutments, material: titanium alloy.',
+//                   price: {
+//                     amount: 70,
+//                     currency: 'EURO',
+//                     validFrom: '',
+//                     validTo: '',
+//                   },
+//                   locale: 'EN',
+//                 },
+//                 {
+//                   name: 'CAMLOG® Stegaufbau, Ø 3.3, gerade, GH 0.5, steril',
+//                   description: '',
+//                   price: {
+//                     amount: 70,
+//                     currency: 'EURO',
+//                     validFrom: '',
+//                     validTo: '',
+//                   },
+//                   locale: 'DE',
+//                 },
+//               ],
+//               abutment: {
+//                 implantLine: 'CAMLOG',
+//                 abutmentLine: 'COMFOUR',
+//                 guided: false,
+//                 platformSwitch: '',
+//                 retention: '',
+//                 material: 'TITANIUM',
+//                 sterile: true,
+//                 connectionType: '',
+//                 heightProsthetic: 2.5,
+//                 maxTorque: 20,
+//                 angle: 0,
+//                 diameterPlatform: '',
+//                 heightGingiva: 0.5,
+//                 indication: 'BRIDGE',
+//                 singleUse: true,
+//                 workflow: 'ANALOG',
+//               },
+//             },
+//             quantity: 1,
+//           },
+//         ],
+//       },
+//     },
+//     root: {
+//       treatmentDoc: {
+//         treatment: {
+//           name: 'adult',
+//         },
+//         selectedProducts: [],
+//       },
+//     },
+//   },
+//   {
+//     name: '16',
+//     crown: {
+//       treatmentDoc: {
+//         treatment: {
+//           name: 'bridgeAnchor',
+//         },
+//         selectedProducts: [
+//           {
+//             selectedProduct: {
+//               id: '369977683994476749',
+//               coll: 'Product',
+//               ts: '2023-07-11T18:04:05.970Z',
+//               manufacturer: {
+//                 name: 'CAMLOG',
+//               },
+//               manufacturerProductId: 'J2244.3302',
+//               image: 'https://bucket.dechea.com/product/images/J2244.3302.jpg',
+//               validFrom: '',
+//               validTo: '',
+//               status: 'ACTIVE',
+//               localizations: [
+//                 {
+//                   name: '',
+//                   description: '',
+//                   price: {
+//                     amount: 18,
+//                     currency: 'EURO',
+//                     validFrom: '',
+//                     validTo: '',
+//                   },
+//                   locale: 'FR',
+//                 },
+//                 {
+//                   name: 'CAMLOG® Modeling aid for CAMLOG® Titanium base CAD/CAM, crown, Ø 3.3, burn-out',
+//                   description:
+//                     'CAMLOG® Modeling aid for CAMLOG® Titanium base CAD/CAM, burn-out, for fabricating mesostructures and crowns, material: POM.',
+//                   price: {
+//                     amount: 18,
+//                     currency: 'EURO',
+//                     validFrom: '',
+//                     validTo: '',
+//                   },
+//                   locale: 'EN',
+//                 },
+//                 {
+//                   name: 'CAMLOG® Modellierhilfe für CAMLOG® Titanbasis CAD/CAM, Krone, Ø 3.3, ausbrennbar',
+//                   description: '',
+//                   price: {
+//                     amount: 18,
+//                     currency: 'EURO',
+//                     validFrom: '',
+//                     validTo: '',
+//                   },
+//                   locale: 'DE',
+//                 },
+//               ],
+//               abutment: {
+//                 implantLine: 'CAMLOG',
+//                 abutmentLine: '',
+//                 guided: false,
+//                 platformSwitch: '',
+//                 retention: '',
+//                 material: '',
+//                 sterile: false,
+//                 connectionType: '',
+//                 heightProsthetic: '',
+//                 maxTorque: '',
+//                 angle: '',
+//                 diameterPlatform: '',
+//                 heightGingiva: '',
+//                 indication: 'CROWN',
+//                 singleUse: true,
+//                 workflow: 'ANALOG',
+//               },
+//             },
+//             quantity: 1,
+//           },
+//         ],
+//       },
+//     },
+//     root: {
+//       treatmentDoc: {
+//         treatment: {
+//           name: 'adult',
+//         },
+//         selectedProducts: [],
+//       },
+//     },
+//   },
--- components/CarouselTooth/index.tsx
+++ components/CarouselTooth/index.tsx
@@ -1,18 +1,19 @@
 'use client';
 
-import { View, Icon } from 'reshaped';
+import { Actionable, Icon, Image, Popover, Text, View } from 'reshaped';
+import { selectedTeethData } from '../../__mocks__/treatmentTabs';
+import { SelectedProduct } from '../../fqlx-generated/typedefs';
+import { useProductStore } from '../../zustand/product';
+import { useTeethDiagramStore } from '../../zustand/teethDiagram';
+import TickIcon from '../Icons/Tick';
+import Crown from '../TeethDiagram/teeth/areas/crown-side-view';
+import Root from '../TeethDiagram/teeth/areas/root';
 import {
   Tooth,
   ToothContainer,
   ToothNumber,
 } from '../TeethDiagram/teeth/areas/tooth';
-import Root from '../TeethDiagram/teeth/areas/root';
-import Crown from '../TeethDiagram/teeth/areas/crown-side-view';
 import { JawType } from '../TeethDiagram/teeth/areas/tooth/interfaces/props';
-import TickIcon from '../Icons/Tick';
-import { useTeethDiagramStore } from '../../zustand/teethDiagram';
-import { useProductStore } from '../../zustand/product';
-
 interface CarouselToothProps {
   toothNumber: number;
 }
@@ -26,42 +27,106 @@ export const CarouselTooth = ({ toothNumber }: CarouselToothProps) => {
   const { treatments } = useTeethDiagramStore((state) => state);
   const { selectedProducts } = useProductStore((state) => state);
 
-  const toothData = treatments[toothNumber as keyof typeof treatments] || {};
+  const toothData = treatments[toothNumber as keyof typeof treatments] ?? {};
   const selected = !!selectedProducts[toothNumber];
 
   return (
-    <View height={30} width={15} aspectRatio={60 / 120}>
-      <ToothContainer customStyles='!w-full pointer-events-none'>
-        <Tooth
-          variant={toothData?.toothVariant}
-          tooth={toothNumber}
-          className={selected ? styles.activeTooth : styles.inactiveTooth}
-        >
-          <Root tooth={toothNumber} variant={toothData.rootVariant} />
-          <Crown
-            tooth={toothNumber}
-            variant={toothData.crownVariant}
-            leftAnchor={toothData.leftAnchor}
-            rightAnchor={toothData.rightAnchor}
-          />
-          {selected && (
-            <View
-              position='absolute'
-              width='100%'
-              height='100%'
-              direction='row'
-              insetTop={3}
-              align='center'
-              justify='center'
-              className='[&_svg]:!opacity-100 [&_svg>*]:!stroke-[--rs-color-background-page] [&_svg>*]:!fill-[--rs-color-background-primary]'
-            >
-              <Icon svg={TickIcon} size={6} />
-            </View>
-          )}
-        </Tooth>
+    <>
+      <Popover triggerType='hover' padding={3}>
+        <Popover.Trigger>
+          {(attributes) => (
+            <Actionable attributes={attributes}>
+              <View height={30} width={15} aspectRatio={60 / 120}>
+                <ToothContainer customStyles='!w-full pointer-events-none'>
+                  <Tooth
+                    variant={toothData?.toothVariant}
+                    tooth={toothNumber}
+                    className={
+                      selected ? styles.activeTooth : styles.inactiveTooth
+                    }
+                  >
+                    <Root tooth={toothNumber} variant={toothData.rootVariant} />
+                    <Crown
+                      tooth={toothNumber}
+                      variant={toothData.crownVariant}
+                      leftAnchor={toothData.leftAnchor}
+                      rightAnchor={toothData.rightAnchor}
+                    />
+                    {selected && (
+                      <View
+                        position='absolute'
+                        width='100%'
+                        height='100%'
+                        direction='row'
+                        insetTop={3}
+                        align='center'
+                        justify='center'
+                        className='[&_svg]:!opacity-100 [&_svg>*]:!stroke-[--rs-color-background-page] [&_svg>*]:!fill-[--rs-color-background-primary]'
+                      >
+                        <Icon svg={TickIcon} size={6} />
+                      </View>
+                    )}
+                  </Tooth>
 
-        <ToothNumber tooth={toothNumber} jawType={JawType.UPPER} />
-      </ToothContainer>
-    </View>
+                  <ToothNumber tooth={toothNumber} jawType={JawType.UPPER} />
+                </ToothContainer>
+              </View>
+            </Actionable>
+          )}
+        </Popover.Trigger>
+        <Popover.Content>
+          {selectedTeethData.map((tooth) => {
+            const products: SelectedProduct[] = [
+              ...(tooth.root.treatmentDoc?.selectedProducts ?? []),
+              ...(tooth.crown.treatmentDoc?.selectedProducts ?? []),
+            ];
+
+            if (products.length == 0) {
+              return;
+            }
+
+            return (
+              <View divided gap={4.25}>
+                {products.map(({ selectedProduct, quantity }) => {
+                  const localization = selectedProduct?.localizations?.[1];
+
+                  return (
+                    <View
+                      key={selectedProduct?.id as string}
+                      align='center'
+                      direction='row'
+                      gap={4}
+                      wrap={false}
+                    >
+                      <Image
+                        src={selectedProduct?.image}
+                        alt={selectedProduct?.image}
+                        height='48px'
+                        width='48px'
+                        borderRadius='medium'
+                      />
+
+                      <View gap={1} width='232px' textAlign='start'>
+                        <Text variant='body-3' weight='bold'>
+                          {localization?.name}
+                        </Text>
+
+                        <Text
+                          variant='caption-1'
+                          weight='regular'
+                          color='neutral-faded'
+                        >
+                          {localization?.description}
+                        </Text>
+                      </View>
+                    </View>
+                  );
+                })}
+              </View>
+            );
+          })}
+        </Popover.Content>
+      </Popover>
+    </>
   );
 };
