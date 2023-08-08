7978840a06e8f4cbbcfae914d0535ffe193e5392
MananSoniDechea <manan.soni@dechea.com>
2023-08-08 13:26:23 +0000

    Remove product filters

--- app/[patientFileId]/treatments/abutment/page.tsx
+++ app/[patientFileId]/treatments/abutment/page.tsx
@@ -3,10 +3,8 @@
 import { Suspense, useEffect } from 'react';
 import { Tabs, View } from 'reshaped';
 import ProductList from '../../../../components/ProductList';
-import { ProductFilterForm } from '../../../../components/ProductFilterForm';
 import CarouselTeeth from '../../../../components/CarouselTeeth';
 import Loader from '../../../../components/Loader';
-import { filterCategories } from './filterCategories';
 import {
   AREA_TYPE,
   TREATMENT_GROUP,
@@ -39,41 +37,19 @@ export default function Abutment({
   return (
     <Tabs.Panel value={`/${params.patientFileId}/treatments/abutment`}>
       <CarouselTeeth />
-      <View direction="column" width="100%" align="center">
+      <View direction='column' width='100%' align='center'>
         <View
-          direction="row"
+          direction='row'
           gap={12}
-          maxWidth="1280px"
-          width="100%"
-          justify="center"
+          maxWidth='1280px'
+          width='100%'
+          justify='center'
         >
-          <View.Item columns={3} className="sticky !top-[180px]">
-            <View
-              paddingStart={6}
-              paddingTop={8}
-              height="calc(100vh - 240px)"
-              className="overflow-y-auto scrollbar-0"
-            >
-              <Suspense
-                fallback={
-                  <View height="70vh">
-                    <Loader />
-                  </View>
-                }
-              >
-                <ProductFilterForm
-                  filterCategories={filterCategories}
-                  productType={PRODUCT_TYPE.ABUTMENT}
-                />
-              </Suspense>
-            </View>
-          </View.Item>
-
           <View.Item columns={9}>
             <View paddingEnd={6} paddingTop={8}>
               <Suspense
                 fallback={
-                  <View height="70vh">
+                  <View height='70vh'>
                     <Loader />
                   </View>
                 }
