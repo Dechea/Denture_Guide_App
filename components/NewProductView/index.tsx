345ff4a2d700a0f777ad648609cf3bb9ab147848
MadhavKD <madhav.kabra@dechea.com>
2023-08-16 06:41:37 +0000

    Add skeleton loader

--- components/NewProductView/index.tsx
+++ components/NewProductView/index.tsx
@@ -2,7 +2,7 @@
 
 import { useQuery } from 'fqlx-client';
 import { Suspense, useEffect, useMemo } from 'react';
-import { Badge, Card, Text, View } from 'reshaped';
+import { Badge, Card, Skeleton, Text, View } from 'reshaped';
 import { Query, Tooth } from '../../fqlx-generated/typedefs';
 import { useProductCrudOps } from '../../hooks/useProductCrudOps';
 import { useTreatmentsByGroup } from '../../hooks/useTreatmentsByGroup';
@@ -12,7 +12,6 @@ import { AREA_TYPE, PRODUCT_TYPE } from '../../zustand/product/interface';
 import { formWhereCondition } from './helper';
 import NewProductToothList from '../NewProductToothList';
 import NewProductCard from '../NewProductCard';
-import Loader from '../Loader';
 
 interface Field {
   id: string;
@@ -177,8 +176,50 @@ const NewProductView = ({
             <View.Item columns={{ s: 12, m: 8 }}>
               <Suspense
                 fallback={
-                  <View height="70vh">
-                    <Loader />
+                  <View padding={6} paddingBottom={10}>
+                    <View direction="row" gap={6}>
+                      <Skeleton
+                        width={{ s: '120px', l: '140px' }}
+                        height={{ s: '120px', l: '140px' }}
+                      />
+
+                      <View gap={2} grow>
+                        <View
+                          direction="row"
+                          align="start"
+                          className="!justify-between"
+                        >
+                          <Skeleton width="100%" height="50px" />
+                        </View>
+                        <View direction="row" gap={4}>
+                          <Skeleton width="100%" />
+                        </View>
+                      </View>
+                    </View>
+                    <View.Item grow>
+                      <View
+                        gap={6}
+                        paddingStart={{ l: 41 }}
+                        paddingTop={{ s: 8, l: 0 }}
+                      >
+                        <Skeleton width="100%" height="46px" />
+                        <Skeleton width="100%" height="46px" />
+                        <Skeleton width="140px" height="20px" />
+                        <View direction="row" gap={4}>
+                          <View.Item columns={6}>
+                            <Skeleton height="56px" />
+                          </View.Item>
+                          <View.Item columns={6}>
+                            <Skeleton height="56px" />
+                          </View.Item>
+                        </View>
+                        <View direction="row" gap={4}>
+                          <View.Item columns={6}>
+                            <Skeleton height="56px" />
+                          </View.Item>
+                        </View>
+                      </View>
+                    </View.Item>
                   </View>
                 }
               >
