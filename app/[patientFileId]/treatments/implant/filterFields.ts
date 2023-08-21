912823a716ffa49c1ce41ad051235e0a14ff4ea3
MananSoniDechea <manan.soni@dechea.com>
2023-08-21 08:25:30 +0000

    Update filter order for implant

--- app/[patientFileId]/treatments/implant/filterFields.ts
+++ app/[patientFileId]/treatments/implant/filterFields.ts
@@ -9,9 +9,24 @@ export const implantProductFields: {
     type: 'tabs',
   },
   {
+    id: 'c3b1c3da-3808-47bd-8fbf-d6c7e873d6c1',
+    name: 'diameterPlatform',
+    type: 'dropdown',
+  },
+  {
     id: '5901f5e4f-35b9-4161-bada-b73b617da1f4',
     name: 'useCase',
     type: 'tabs',
+  },
+  {
+    id: 'e00deddd-87ff-4ef6-9675-e3f99791f1d5',
+    name: 'length',
+    type: 'dropdown',
+  },
+  {
+    id: 'e00deddd-87ff-47bd-8fbf-d6c7e873d6c1',
+    name: 'lengthNeck',
+    type: 'dropdown',
   },
   {
     id: 'cf3ddee0-06a4-4e26-b4ee-fb3858070c23',
@@ -26,21 +41,6 @@ export const implantProductFields: {
   {
     id: '8f8cda28-dc23-4200-a18e-4c88672d14f6',
     name: 'material',
-    type: 'dropdown',
-  },
-  {
-    id: 'e00deddd-87ff-4ef6-9675-e3f99791f1d5',
-    name: 'length',
-    type: 'dropdown',
-  },
-  {
-    id: 'e00deddd-87ff-47bd-8fbf-d6c7e873d6c1',
-    name: 'lengthNeck',
-    type: 'dropdown',
-  },
-  {
-    id: 'c3b1c3da-3808-47bd-8fbf-d6c7e873d6c1',
-    name: 'diameterPlatform',
     type: 'dropdown',
   },
 ];
