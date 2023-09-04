bf624a967326b0f37acf1374ab58e277c3561637
MananSoniDechea <manan.soni@dechea.com>
2023-09-04 08:51:16 +0000

    Responsive CTA for implant and abutment

--- components/TreatmentTabsPopover/index.tsx
+++ components/TreatmentTabsPopover/index.tsx
@@ -1,4 +1,4 @@
-import { Actionable, Image, Popover, Text, View } from 'reshaped';
+import { Actionable, Hidden, Image, Popover, Text, View } from 'reshaped';
 
 interface TreatmentTabsPopoverProps {
   activeTab: boolean;
@@ -34,33 +34,53 @@ export default function TreatmentTabsPopover({
           </Actionable>
         )}
       </Popover.Trigger>
-      <Popover.Content className='mt-[10px]'>
-        <View
-          gap={4}
-          padding={2}
-          paddingEnd={4}
-          direction='row'
-          align='center'
-          backgroundColor='black'
-        >
-          <Image
-            src={image}
-            alt={title}
-            width='107px'
-            height='80px'
-            borderRadius='medium'
-          />
+      <Popover.Content className='mt-[10px] max-[900px]:fixed max-[900px]:left-0 max-[900px]:bottom-0 max-[900px]:!border-none'>
+        <Hidden hide={{ s: true, m: false }}>
+          <View
+            gap={4}
+            padding={2}
+            paddingEnd={4}
+            direction='row'
+            align='center'
+            backgroundColor='black'
+          >
+            <Image
+              src={image}
+              alt={title}
+              width='107px'
+              height='80px'
+              borderRadius='medium'
+            />
 
-          <View width='253px'>
-            <Text
-              variant='body-2'
-              weight='regular'
-              className='text-[--rs-color-white]'
-            >
-              {description}
-            </Text>
+            <View width='253px'>
+              <Text
+                variant='body-2'
+                weight='regular'
+                className='text-[--rs-color-white]'
+              >
+                {description}
+              </Text>
+            </View>
           </View>
-        </View>
+        </Hidden>
+        <Hidden hide={{ s: false, m: true }}>
+          <View width='100vw' align={'center'} paddingBottom={5}>
+            <View
+              padding={4}
+              maxWidth={82}
+              backgroundColor='black'
+              borderRadius={'large'}
+            >
+              <Text
+                variant='body-2'
+                weight='regular'
+                className='text-[--rs-color-white]'
+              >
+                {description}
+              </Text>
+            </View>
+          </View>
+        </Hidden>
       </Popover.Content>
     </Popover>
   );
