279309918480d8769133f3a032d575b5bbdd75c8
MananSoniDechea <manan.soni@dechea.com>
2023-09-05 10:50:40 +0000

    orders header

--- components/OrderDashboardHeader/index.tsx
+++ components/OrderDashboardHeader/index.tsx
@@ -5,20 +5,18 @@ import { Actionable, Image, View } from 'reshaped';
 
 export default function OrderDashboardHeader() {
   return (
-    <View>
-      <View
-        direction='row'
-        align='center'
-        padding={4}
-        borderColor='neutral-faded'
-        className='!justify-between items-center border-b-0 bg-[var(--rs-color-background-neutral-faded)]'
-      >
-        <Image src='/decheaLogo.svg' alt='Dechea' height={'24px'} />
-        <View>
-          <Actionable>
-            <UserButton />
-          </Actionable>
-        </View>
+    <View
+      direction='row'
+      align='center'
+      padding={4}
+      borderColor='neutral-faded'
+      className='!justify-between border-t-0 border-l-0 border-r-0 items-center bg-[var(--rs-color-background-neutral-faded)]'
+    >
+      <Image src='/decheaLogo.svg' alt='Dechea' height={'24px'} />
+      <View>
+        <Actionable>
+          <UserButton />
+        </Actionable>
       </View>
     </View>
   );
