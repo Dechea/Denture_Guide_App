ba8462cec28b7a4b7feaa741fea0d2ab3d8e92e8
bohdank1995 <bohdan@karpliak.me>
2023-09-06 15:14:34 +0000

    Update patient name alignment. Recreated header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -35,53 +35,59 @@ export default function Header({ patientFileId }: HeaderProps) {
 
   return (
     <View
-      align='stretch'
-      width='100%'
-      position='sticky'
+      align="stretch"
+      width="100%"
+      position="sticky"
       insetTop={0}
       zIndex={50}
-      backgroundColor='white'
-      className='print:!hidden'
+      backgroundColor="white"
+      className="print:!hidden"
     >
-      <View
-        direction='row'
-        align='center'
-        paddingBlock={{ s: 1, l: 2 }}
-        paddingInline={{ s: 2, l: 6 }}
-        className='!justify-between'
-      >
-        <Button
-          icon={<Icon svg={MenuIcon} size={4} />}
-          variant='ghost'
-          onClick={() => router.push('/')}
-        >
-          <Hidden hide={{ s: true, l: false }}>
-            <Text variant='body-3' weight='medium'>
-              Orders List
+      <div className="flex flex-row px-x4 py-x2">
+        <div className="basis-1/3 align-middle">
+          <Button
+            icon={<Icon svg={MenuIcon} size={4} />}
+            variant="ghost"
+            onClick={() => router.push('/')}
+          >
+            <Hidden hide={{ s: true, l: false }}>
+              <Text variant="body-3" weight="medium">
+                Orders List
+              </Text>
+            </Hidden>
+            <Hidden hide={{ s: false, l: true }}>
+              <Text variant="body-3" weight="medium">
+                Orders
+              </Text>
+            </Hidden>
+          </Button>
+        </div>
+        <div className="basis-1/3 ">
+          <div className="flex h-full items-center w-full ">
+            <Text
+              variant="body-3"
+              weight="medium"
+              className="truncate w-full text-center"
+            >
+              {patientFile?.patient?.name}
             </Text>
-          </Hidden>
-          <Hidden hide={{ s: false, l: true }}>
-            <Text variant='body-3' weight='medium'>
-              Orders
-            </Text>
-          </Hidden>
-        </Button>
-
-        <View direction='row' gap={3} align='center' maxWidth={'50%'}>
-          <Text variant='body-3' weight='medium' className='truncate'>
-            {patientFile?.patient?.name}
-          </Text>
-        </View>
-
-        <View direction='row' align='center' justify='end' gap={{ s: 0, m: 6 }}>
-          <View direction='row' align='center' gap={2.5}>
+          </div>
+        </div>
+        <div className="basis-1/3 ">
+          <View
+            direction="row"
+            align="center"
+            width="100%"
+            justify="end"
+            gap={2.5}
+          >
             <Button
-              variant='ghost'
+              variant="ghost"
               icon={
-                <Badge.Container className='w-8 pr-x2'>
+                <Badge.Container className="w-8 pr-x2">
                   <Hidden hide={{ s: false, m: true }}>
-                    <Badge className='!bg-neutral' size='small'>
-                      <Text className='!text-neutral'>
+                    <Badge className="!bg-neutral" size="small">
+                      <Text className="!text-neutral">
                         {totalProductsInCart}
                       </Text>
                     </Badge>
@@ -94,12 +100,12 @@ export default function Header({ patientFileId }: HeaderProps) {
             >
               <>
                 <Hidden hide={{ s: true, m: false }}>
-                  <View direction='row' align='center' justify='center' gap={3}>
-                    <Text variant='body-3' weight='medium'>
+                  <View direction="row" align="center" justify="center" gap={3}>
+                    <Text variant="body-3" weight="medium">
                       {totalCostOfProductsInCart} €
                     </Text>
-                    <Badge className='!bg-neutral' size='small'>
-                      <Text className='!text-neutral'>
+                    <Badge className="!bg-neutral" size="small">
+                      <Text className="!text-neutral">
                         {totalProductsInCart}
                       </Text>
                     </Badge>
@@ -108,17 +114,92 @@ export default function Header({ patientFileId }: HeaderProps) {
               </>
             </Button>
           </View>
+        </div>
+        {/* <View
+          direction="row"
+          align="center"
+          paddingBlock={{ s: 1, l: 2 }}
+          paddingInline={{ s: 2, l: 6 }}
+          className="!justify-between"
+        >
           <Button
-            icon={<CostEst />}
-            variant='ghost'
-            color='neutral'
-            highlighted={pathname === `/${patientFileId}/cost-estimation`}
-            onClick={() => router.push(`/${patientFileId}/cost-estimation`)}
+            icon={<Icon svg={MenuIcon} size={4} />}
+            variant="ghost"
+            onClick={() => router.push('/')}
           >
-            <Hidden hide={{ s: true, m: false }}>Cost Estimation</Hidden>
+            <Hidden hide={{ s: true, l: false }}>
+              <Text variant="body-3" weight="medium">
+                Orders List
+              </Text>
+            </Hidden>
+            <Hidden hide={{ s: false, l: true }}>
+              <Text variant="body-3" weight="medium">
+                Orders
+              </Text>
+            </Hidden>
           </Button>
-        </View>
-      </View>
+          <View direction="row" gap={3} align="center" maxWidth={'50%'}>
+            <Text variant="body-3" weight="medium" className="truncate">
+              {patientFile?.patient?.name}
+            </Text>
+          </View>
+          <View
+            direction="row"
+            align="center"
+            justify="end"
+            gap={{ s: 0, m: 6 }}
+          >
+            <View direction="row" align="center" gap={2.5}>
+              <Button
+                variant="ghost"
+                icon={
+                  <Badge.Container className="w-8 pr-x2">
+                    <Hidden hide={{ s: false, m: true }}>
+                      <Badge className="!bg-neutral" size="small">
+                        <Text className="!text-neutral">
+                          {totalProductsInCart}
+                        </Text>
+                      </Badge>
+                    </Hidden>
+                    <Icon svg={CartIcon} size={4} />
+                  </Badge.Container>
+                }
+                highlighted={pathname === `/${patientFileId}/cart`}
+                onClick={() => router.push(`/${patientFileId}/cart`)}
+              >
+                <>
+                  <Hidden hide={{ s: true, m: false }}>
+                    <View
+                      direction="row"
+                      align="center"
+                      justify="center"
+                      gap={3}
+                    >
+                      <Text variant="body-3" weight="medium">
+                        {totalCostOfProductsInCart} €
+                      </Text>
+                      <Badge className="!bg-neutral" size="small">
+                        <Text className="!text-neutral">
+                          {totalProductsInCart}
+                        </Text>
+                      </Badge>
+                    </View>
+                  </Hidden>
+                </>
+              </Button>
+            </View>
+            <Button
+              icon={<CostEst />}
+              variant="ghost"
+              color="neutral"
+              highlighted={pathname === `/${patientFileId}/cost-estimation`}
+              onClick={() => router.push(`/${patientFileId}/cost-estimation`)}
+            >
+              <Hidden hide={{ s: true, m: false }}>Cost Estimation</Hidden>
+            </Button>
+          </View>
+        </View> */}
+      </div>
       <Divider />
     </View>
   );
