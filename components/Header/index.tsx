b633c92206ffa9b90ed6cdabdebe8422e190dfb9
MananSoniDechea <manan.soni@dechea.com>
2023-09-11 07:29:27 +0000

    Remove unused import

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,7 +6,6 @@ import { useMemo } from 'react';
 
 import { Badge, Button, Divider, Hidden, Icon, Text, View } from 'reshaped';
 import { Query } from '../../fqlx-generated/typedefs';
-import CostEst from '../Icons/CostEst';
 import MenuIcon from '../Icons/MenuIcon';
 import CartIcon from '../Icons/Cart';
 import { useProductCalculations } from '../../hooks/useProductCalculations';
@@ -35,59 +34,59 @@ export default function Header({ patientFileId }: HeaderProps) {
 
   return (
     <View
-      align="stretch"
-      width="100%"
-      position="sticky"
+      align='stretch'
+      width='100%'
+      position='sticky'
       insetTop={0}
       zIndex={50}
-      backgroundColor="white"
-      className="print:!hidden"
+      backgroundColor='white'
+      className='print:!hidden'
     >
-      <div className="flex flex-row px-x4 py-x2 bg-page-faded">
-        <div className="basis-1/3 align-middle">
+      <div className='flex flex-row px-x4 py-x2 bg-page-faded'>
+        <div className='basis-1/3 align-middle'>
           <Button
             icon={<Icon svg={MenuIcon} size={4} />}
-            variant="ghost"
+            variant='ghost'
             onClick={() => router.push('/')}
           >
             <Hidden hide={{ s: true, l: false }}>
-              <Text variant="body-3" weight="medium">
+              <Text variant='body-3' weight='medium'>
                 Orders List
               </Text>
             </Hidden>
             <Hidden hide={{ s: false, l: true }}>
-              <Text variant="body-3" weight="medium">
+              <Text variant='body-3' weight='medium'>
                 Orders
               </Text>
             </Hidden>
           </Button>
         </div>
-        <div className="basis-1/3 ">
-          <div className="flex h-full items-center w-full ">
+        <div className='basis-1/3 '>
+          <div className='flex h-full items-center w-full '>
             <Text
-              variant="body-3"
-              weight="medium"
-              className="truncate w-full text-center"
+              variant='body-3'
+              weight='medium'
+              className='truncate w-full text-center'
             >
               {patientFile?.patient?.name}
             </Text>
           </div>
         </div>
-        <div className="basis-1/3 ">
+        <div className='basis-1/3 '>
           <View
-            direction="row"
-            align="center"
-            width="100%"
-            justify="end"
+            direction='row'
+            align='center'
+            width='100%'
+            justify='end'
             gap={2.5}
           >
             <Button
-              variant="ghost"
+              variant='ghost'
               icon={
-                <Badge.Container className="w-8 pr-x2">
+                <Badge.Container className='w-8 pr-x2'>
                   <Hidden hide={{ s: false, m: true }}>
-                    <Badge className="!bg-neutral" size="small">
-                      <Text className="!text-neutral">
+                    <Badge className='!bg-neutral' size='small'>
+                      <Text className='!text-neutral'>
                         {totalProductsInCart}
                       </Text>
                     </Badge>
@@ -100,12 +99,12 @@ export default function Header({ patientFileId }: HeaderProps) {
             >
               <>
                 <Hidden hide={{ s: true, m: false }}>
-                  <View direction="row" align="center" justify="center" gap={3}>
-                    <Text variant="body-3" weight="medium">
+                  <View direction='row' align='center' justify='center' gap={3}>
+                    <Text variant='body-3' weight='medium'>
                       {totalCostOfProductsInCart} €
                     </Text>
-                    <Badge className="!bg-neutral" size="small">
-                      <Text className="!text-neutral">
+                    <Badge className='!bg-neutral' size='small'>
+                      <Text className='!text-neutral'>
                         {totalProductsInCart}
                       </Text>
                     </Badge>
