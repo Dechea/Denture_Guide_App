41065e7e807c69a2e702c70698956a022991c786
Micha Mailänder <micha@mailaender.me>
2023-09-14 09:31:45 +0000

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,9 @@
-import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  afterAuth(auth, req, evt) {
-    // handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-  },
-  publicRoutes: ['/sign-in', '/sign-up'],
+  publicRoutes: ["/sign-in(.*)", "/sign-up(.*)"],
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
