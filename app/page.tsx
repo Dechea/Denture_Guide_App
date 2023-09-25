dba0148c3f856d13abeb5af2edb8bd272dd9640a
MananSoniDechea <manan.soni@dechea.com>
2023-09-25 05:19:58 +0000

    Complete user redirect if not in fauna

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 'use client';
 
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useMemo } from 'react';
 import OrderDashboard from '../components/OrderDashboard';
 import Loader from '../components/Loader';
+import { useUser } from '@clerk/nextjs';
+import { useQuery } from 'fauna-typed';
+import { Query } from '../fqlx-generated/typedefs';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
+  const { user } = useUser();
+  const query = useQuery<Query>();
+
+  const faunaUser = useMemo(
+    () => query.User.firstWhere(`user => user.clerkId == "${user?.id}"`).exec(),
+    [user]
+  );
+
+  useEffect(() => {
+    if (faunaUser === null) {
+      redirect('/users/sync');
+    }
+  }, []);
+
   return (
     <Suspense fallback={<Loader />}>
       <OrderDashboard />
